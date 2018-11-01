16de05259950eea7f558b1d10a215ef7799eb2ba
min <mjnorthwesternwork1121@gmail.com>
2018-10-31 22:05:04 -0500

    Made and added psuedo code. Started timer syntax codeing.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,62 @@
-// Step one: Welcome player to the game.
-.alert('Your book is overdue.'); 
-//General layout
+// Step one: Welcome player to the gam
+$ .alert('Hello'); 
+
+
+//Start button displayed
+    //Once button clicked pressed...
+    
+    //Alert that notify the player the timer starts after pressing okay.
+
+    //Once the player presses okay, the timer starts countdown
+
+//Timer Section
+
+window.onload = funtion() {
+    $("#start").(timer.start)
+}
+function timeUp() {
+  // in the element with an id of time-left add an h2 saying Time's Up!
+  // console log done
+  console.log("done");
+  $("#time-left").append("<h2>Time's Up!</h2>");
+  console.log("time is up");
+
+
+ //Questions section
+
+ var questions = {
+
+ }
+
+    //Add multiple choice boxes
+
+    //Have boxes checked
+
+// If the count down is finished... 
+
+    // alert comes up to tell user time is up
+
+    // once game ended
+
+    
+// Have results graded
+   
+    //If question is left blank...
+    
+    //if questions are wrong 
+
+    //If questions are correct
+
+
+//The Results Section
+
+    //Correct amount of answers displayed
+
+    //true amount of answers displayed
+
+//Extra features
+    
+    //audio for game playing on a loop
+        
+    //Button to stop the game
+
