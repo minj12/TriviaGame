c5f0f8fc8b8139f609453edca5855fe0a5057c24
min <mjnorthwesternwork1121@gmail.com>
2018-11-03 08:20:44 -0500

    Saw code errors and decided to redo my syntax code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,97 +1,23 @@
-// Step one: Welcome player to the game
-$ .alert('Hello'); 
+//Data for timer
 
+$(document).ready(function)() {
+    var index = 0;
+    var countdownTimer; = {
+        time : 30,
+        reset: function() {
+            this.time = 30;
+            $('.timer').html('<h2>' + this.time + ' seconds remaining </h2>');//sets timer font
+        },
+        start: function() {
+            counter = setInterval(countdownTimer.count, 1000);
+        },
+        stop: function() {          //
+            clearInterval(counter);
+        },
+        count: function() {
+            countdownTimer.time--;
+            console.log(countdownTimer.time);
 
-//Start button displayed
-    //Once button clicked pressed...
-    
-    //Alert that notify the player the timer starts after pressing okay.
-
-    //Once the player presses okay, the timer starts countdown
-
-//Timer Section
-    //Set the number to 1:00 minute
-    var Timer = 01:00;
-    var intervalId;
-
-    function run() {
-        clearInterval(intervalid);
-        intervalid = setInterval(decrement,1000)
-    }
-
-    // The decrement function.
-    function decrement() {
-        number--;
-        $("#show-number").html("<h2>" + number + "</h2>")
-
-        if(number ===0) {
-        stop();
-        alert("time Up!");
         }
     }
-    
-    // Stopping the function
-    function stop() {
-
-        //Clears our intervalId
-    clearInterval(intervalId);
-    }
-
-    //Start the function.
-    run();
-
-    console.log(timer)
-
-//**Feedback or understand  */CHECKOUt DOCUMRNT READY
-//CLICK EVENT
-window.onload = funtion() {
-    $("#start").(timer.start)
 }
-function timeUp() {
-  // in the element with an id of time-left add an h2 saying Time's Up!
-  console.log("done");
-  $("#time-left").append("<h2>Time's Up!</h2>");
-  console.log("time is up");
-
-
- //Questions section
-
- var questions = {
-    //questions go in here
- }
-
-    //Add multiple choice boxes
-
-    //Have boxes checked
-
-// If the count down is finished... 
-
-    // alert comes up to tell user time is up
-
-    // once game ended
-
-    
-// Have results graded
-var Results = {
-
-}
-   
-    //If question is left blank...
-    
-    //if questions are wrong 
-
-    //If questions are correct
- 
-
-//The Results Section
-
-    //Correct amount of answers displayed
-
-    //true amount of answers displayed
-
-//Extra features
-    
-    //audio for game playing on a loop
-        
-    //Button to stop the game
-
